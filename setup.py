ea028fb52c85964dd163d8fb2e557539721028b8
Lekuru <contact@lekuru.xyz>
2023-07-01 22:48:22 +0200

    Fix 'long_description' content type

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setuptools.setup(
     author_email="contact@lekuru.xyz",
     description="A python library that emulates the osu! stable client",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=["osu", "osu/api", "osu/bancho", "osu/objects"],
     install_requires=["requests", "psutil", "python-dateutil"],
     keywords=["osu", "osugame", "python", "bancho"],
