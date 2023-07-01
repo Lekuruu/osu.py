ce107567f8103744d7825b61ba9060a58db6796e
Lekuru <contact@lekuru.xyz>
2023-07-01 22:39:00 +0200

    Add readme as long_description

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 import setuptools
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="osu",
     version="1.0.3",
     author="Lekuru",
     author_email="contact@lekuru.xyz",
     description="A python library that emulates the osu! stable client",
-    long_description="osu.py is a python library that emulates part of the online functionality of the osu! stable client.",
+    long_description=long_description,
     packages=["osu"],
-    install_requires=["requests", "psutil"],
+    install_requires=["requests", "psutil", "python-dateutil"],
     keywords=["osu", "osugame", "python", "bancho"],
 )
