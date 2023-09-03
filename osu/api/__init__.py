b7e50baa3c9049876af942ea1c67bc349c8f052f
Lekuru <contact@lekuru.xyz>
2023-09-04 00:12:26 +0200

    Add imports for constants

--- osu/api/__init__.py
+++ osu/api/__init__.py
@@ -1,1 +1,2 @@
+from .constants import RankingType, SubmissionStatus, CommentTarget
 from .client import WebAPI
--- osu/bancho/__init__.py
+++ osu/bancho/__init__.py
@@ -1,5 +1,18 @@
 from .client import BanchoClient
 from .packets import Packets
+from .constants import (
+    PresenceFilter,
+    ClientPackets,
+    ServerPackets,
+    ReplayAction,
+    StatusAction,
+    ButtonState,
+    Privileges,
+    LoginError,
+    Grade,
+    Mods,
+    Mode,
+)
 
 from . import constants
 from . import packets
