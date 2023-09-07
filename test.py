494b392e5083a4c3c15e423cc5d66c5d80f9d556
Lekuru <contact@lekuru.xyz>
2023-09-07 14:16:57 +0200

    Update tests

--- test.py
+++ test.py
@@ -1,5 +1,75 @@
 from osu import Game
+import logging
 
-game = Game("testuser", "testpass")
+logging.basicConfig(
+    level=logging.DEBUG, format="[%(asctime)s] - <%(name)s> %(levelname)s: %(message)s"
+)
 
-# TODO: Tasks, Simulated Events and more checks
+logger = logging.getLogger("tests")
+logger.info("Performing tests...")
+
+game = Game(
+    username="testuser",
+    password="testpass",
+    version="b1337",
+    server="ppy.sh",
+    stream="stable40",
+)
+
+async_task_success = False
+task_success = False
+
+
+def tasks():
+    logger.info("Testing tasks...")
+
+    @game.tasks.register(seconds=0)
+    def test_task():
+        global task_success
+        task_success = True
+
+    @game.tasks.register(seconds=0, threaded=True)
+    def test_task_async():
+        global async_task_success
+        async_task_success = True
+
+    # Bancho needs to be connected for tasks to run
+    game.bancho.connected = True
+
+    # Execute test_task
+    game.tasks.execute()
+
+    if not task_success:
+        logger.error("Syncronous task failed.")
+        exit(1)
+
+    # Execute test_task_async
+    game.tasks.execute()
+
+    # Wait for thread
+    for thread in game.executor._threads:
+        thread.join(timeout=1)
+
+    if not async_task_success:
+        logger.error("Asyncronous task failed.")
+        exit(1)
+
+    logger.info("Tasks were executed successfully.")
+    game.bancho.connected = False
+
+
+def packets():
+    logger.info("Simulating login reply packet...")
+    game.packets.data_received(
+        data=b"\x05\x00\x00\x04\x00\x00\x00\x01\x00\x00\x00", game=game
+    )
+
+    if game.bancho.player.id != 1:
+        logger.error("Login reply packet failed.")
+        exit(1)
+
+    # TODO: More packets?
+
+
+tasks()
+packets()
