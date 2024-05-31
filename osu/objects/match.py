df5b6283076f87954247b8a3d7dc83398deae2f3
Lekuru <contact@lekuru.xyz>
2024-05-31 18:10:59 +0200

    Add base `Slot` & `Match` classes

---
+++ osu/objects/match.py
@@ -0,0 +1,58 @@
+from dataclasses import dataclass
+from typing import List
+
+from .player import Player
+from ..game import Game
+from ..bancho.constants import (
+    SlotStatus,
+    SlotTeam,
+    Mods,
+    MatchType,
+    Mode,
+    MatchScoringTypes,
+    MatchTeamTypes,
+)
+
+
+@dataclass(slots=True)
+class Slot:
+    player_id: int = -1
+    status: SlotStatus = SlotStatus.Locked
+    team: SlotTeam = SlotTeam.Neutral
+    mods: Mods = Mods.NoMod
+
+    @property
+    def has_player(self) -> bool:
+        return SlotStatus.HasPlayer & self.status > 0
+
+    @property
+    def is_open(self) -> bool:
+        return self.status == SlotStatus.Open
+
+    @property
+    def is_ready(self) -> bool:
+        return self.status == SlotStatus.Ready
+
+
+class Match:
+    def __init__(self, game: Game, host: Player, password: str = "") -> None:
+        self.id: int = -1
+        self.name: str = f"{host.name}'s Game"
+        self.password: str = password
+        self.host = host
+        self.game = game
+
+        self.freemod: bool = False
+        self.in_progress: bool = False
+        self.type: MatchType = MatchType.Standard
+        self.mods: Mods = Mods.NoMod
+        self.mode: Mode = Mode.Osu
+        self.scoring_type: MatchScoringTypes = MatchScoringTypes.Score
+        self.team_type: MatchTeamTypes = MatchTeamTypes.HeadToHead
+
+        self.beatmap_text: str = ""
+        self.beatmap_id: int = -1
+        self.beatmap_checksum: str = ""
+
+        self.slots: List[Slot] = []
+        self.seed: int = 0
