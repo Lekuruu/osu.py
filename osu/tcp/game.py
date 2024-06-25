18d873af22647b9506e4510460e3453143da1092
Lekuru <contact@lekuru.xyz>
2024-06-25 04:16:13 +0200

    Add `run_async` function to `TcpGame`

--- osu/tcp/game.py
+++ osu/tcp/game.py
@@ -155,3 +155,10 @@ class TcpGame:
 
         if exit_on_interrupt:
             exit(0)
+
+    async def run_async(self) -> None:
+        """Run the game in an async context"""
+        import asyncio
+
+        loop = asyncio.get_event_loop()
+        await loop.run_in_executor(None, self.run, False, False)
