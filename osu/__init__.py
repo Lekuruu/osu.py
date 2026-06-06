4d4e01f3d69396c9b65c917449e003428b707d21
Lekuru <contact@lekuru.xyz>
2026-06-06 17:13:57 +0200

    Add `osu` package metadata

--- osu/__init__.py
+++ osu/__init__.py
@@ -1,9 +1,14 @@
+
+__author__ = "Lekuru"
+__email__ = "contact@lekuru.xyz"
+__version__ = "1.4.14"
+__license__ = "MIT"
+
 from .bancho.client import BanchoClient
 from .api.client import WebAPI
 from .tcp.game import TcpGame
-from .game import Game
-
 from .bancho import constants
+from .game import Game
 
 from . import objects
 from . import bancho
