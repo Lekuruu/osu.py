414b952105fc8f997c416b3a0d7accb7df6599d7
Lekuru <contact@lekuru.xyz>
2026-06-06 16:39:40 +0200

    Move resolvers to `packet_helpers`

--- osu/bancho/packets.py
+++ osu/bancho/packets.py
@@ -10,6 +10,7 @@ from ..objects.player import Player
 if TYPE_CHECKING:
     from ..game import Game
 
+from .packet_helpers import resolve_match, resolve_message
 from .streams import StreamIn
 from .constants import (
     ServerPackets,
@@ -76,75 +77,6 @@ class PacketHandler:
 
 
 Packets = PacketHandler()
-
-
-def resolve_match(stream: StreamIn, game: "Game") -> Match:
-    match = Match.decode(stream, game)
-    missing_players = []
-
-    for slot in match.used_slots:
-        if slot.player_id <= 0:
-            continue
-
-        if game.bancho.players.by_id(slot.player_id):
-            continue
-
-        game.bancho.players.add(Player(slot.player_id, game=game))
-        missing_players.append(slot.player_id)
-
-    if missing_players:
-        game.bancho.request_presence(missing_players)
-
-    game.bancho.matches.add(match)
-    return match
-
-
-def resolve_message(stream: StreamIn, game: "Game"):
-    sender_name = stream.string()
-    message = stream.string()
-    target_name = stream.string()
-    sender_id = stream.s32()
-
-    # Find player
-    player = game.bancho.players.by_id(sender_id)
-
-    if not player:
-        # Try to find sender by name
-        player = game.bancho.players.by_name(sender_name)
-
-    if not player:
-        player = Player(sender_id, sender_name, game)
-
-        if sender_id:
-            game.bancho.players.add(player)
-            game.bancho.request_presence([sender_id])
-
-    if not player.loaded:
-        # Presence missing
-        player.request_presence()
-
-    sender = player
-
-    # Find target
-    if target_name.startswith("#"):
-        # Public message
-        target = game.bancho.channels.get(target_name) or Channel(target_name, game)
-        return sender, message, target
-
-    # Private message
-    target = game.bancho.players.by_name(target_name)
-
-    if not target and game.bancho.player and target_name == game.bancho.player.name:
-        target = game.bancho.player
-
-    if not target:
-        target = Player(0, target_name, game)
-
-    if not target.loaded:
-        # Presence missing
-        target.request_presence()
-
-    return sender, message, target
 
 
 @Packets.register(ServerPackets.PONG)
@@ -627,7 +559,7 @@ def match_invite(stream: StreamIn, game: "Game"):
         target.logger.info(
             f'<{sender.name}{f" ({sender.id})" if sender.id else ""}> [{target.name}]: "{message}"'
         )
-    
+
     game.events.call(ServerPackets.MATCH_INVITE, sender, message, target)
 
 
